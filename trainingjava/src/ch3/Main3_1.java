8f928c697451fac69f20281be7a0eb2f0c1fb3fa
User <User@host.docker.internal>
2025-03-18 10:09:28 +0900

    昨日のファイルが「ステージされてない変更」に表示されていたので再送いたします。

---
+++ trainingjava/src/ch3/Main3_1.java
@@ -0,0 +1,18 @@
+package ch3;
+
+//天気による行動の変化をJavaで表す
+public class Main3_1 {
+
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDを見ます");
+
+		}
+
+	}
+
+}
