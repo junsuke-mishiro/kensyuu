bdc0696bd90dd7faef9133479f1a0b2f74b21dff
User <User@host.docker.internal>
2025-03-18 11:15:45 +0900

    エラーのご確認をお願いいたします。

---
+++ trainingjava/src/ch3/Main3_5.java
@@ -0,0 +1,30 @@
+package ch3;
+
+//break文を忘れると…
+public class Main3_5 {
+
+	public static void main(String[] args) {
+		System.out.println("あなたの運勢を占います");
+		int fortune = 1;
+		switch (fortune) {
+		case1:
+			System.out.println("大吉");
+			break;
+		
+		case2:
+			System.out.println("中吉");
+			break;
+		
+		case3:
+			System.out.println("吉");
+			break;
+		
+		default:
+			System.out.println("凶");
+		
+		
+		}
+
+	}
+
+}
